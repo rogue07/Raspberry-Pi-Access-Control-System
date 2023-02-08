23cdc96119aab345a2381fda96b53ecadcd59190
Ram Persaud <rogue07@users.noreply.github.com>
2023-02-07 19:52:31 -0500

    Add files via upload
    Updated installer

--- installer.sh
+++ installer.sh
@@ -1,10 +1,4 @@
-sudo apt update -y && sudo apt upgrade -y
-
-sudo apt install python3-pip mariadb-server -y
-
-pip3 install --upgrade setuptools adafruit-python-shell schedule python-crontab adafruit-circuitpython-pn532 sh keyboard mysql-connector-python mariadb
-
-clear
-echo "Creating service"
-
+sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install python3-pip -y && sudo apt install mariadb-server -y
+pip3 install --upgrade setuptools adafruit-python-shell schedule python-crontab adafruit-circuitpython-pn532 sh keyboard mysql-connector-python mysql.connector mariadb
 sudo cp -v scanIn.service /etc/systemd/system/ && sudo systemctl start scanIn.service && sudo systemctl enable scanIn.service
+
